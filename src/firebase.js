2ebbdc96f98a30fb1917ea530ec6b9f92d8a6d3a
WilberC1219 <wilberclaudio@gmail.com>
2023-06-15 17:12:42 -0400

    set up my firebase config

--- src/firebase.js
+++ src/firebase.js
@@ -3,6 +3,15 @@ import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBiNeHKH7wlqojpFaO1xu3XhPNkIR-c6e4",
+  authDomain: "wilchat-e3e1a.firebaseapp.com",
+  projectId: "wilchat-e3e1a",
+  storageBucket: "wilchat-e3e1a.appspot.com",
+  messagingSenderId: "622415891463",
+  appId: "1:622415891463:web:906a178847d6c2fa756ea2",
+  measurementId: "G-0PJVMF0R3M",
+};
 
 // Initialize Firebase, auth, db, and provider
 const app = initializeApp(firebaseConfig);
